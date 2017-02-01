6951c6531f27f5ae59c75b05cb159842cc382a20
Sergii Chervonyi <Bekyts@gmail.com>
2017-02-02 00:13:09 +0200

    Implement simple todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+type TodoObj = {newTodo : string, completed: boolean};
 
 @Component({
   selector: 'my-app',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  { 
   public name: string = 'Angular'; 
+
+  public newTodo: string = '';
+  public todos: TodoObj[] = [];
+  private todoObj: TodoObj;
+
+  public addTodo(event: any): void {
+    this.todoObj = {
+      newTodo : this.newTodo,
+      completed: false
+    }
+    this.todos.push(this.todoObj);
+    this.newTodo = '';
+    event.preventDefault();
+  }
 }
